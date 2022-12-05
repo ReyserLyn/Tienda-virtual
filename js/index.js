daf1d3cf4ffe4f8ac2ceacb4a97f7873e6bdea28
Reyser <rzapata@unsa.edu.pe>
2022-12-05 19:28:59 +0000

    HEADER TERMINADO de la tienda virtual

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-const toggle = document.querySelector(".nav-toggle")
-const menu = document.querySelector(".nav-menu")
+const toggle = document.querySelector(".nav-toggle");
+const menu = document.querySelector(".nav-menu");
+const xmark = document.querySelector(".xmark");
 const incio = document.getElementById("inicio");
 const perifericos = document.getElementById("perifericos");
 const hardware = document.getElementById("hardware");
@@ -11,13 +12,18 @@ toggle.addEventListener("click", ()  => {
     menu.classList.toggle("nav-menu_visible");
 })
 
+xmark.addEventListener("click", ()  => {
+    menu.classList.toggle("nav-menu_visible");
+})
+
 inicio.addEventListener("click", ()  => {
     incio.classList.add("nav-menu-link_active");
 
     perifericos.classList.remove("nav-menu-link_active")
     hardware.classList.remove("nav-menu-link_active")
     software.classList.remove("nav-menu-link_active")
-    contacto.classList.remove("nav-menu-link_active")})
+    contacto.classList.remove("nav-menu-link_active")
+})
 
 perifericos.addEventListener("click", ()  => {
     perifericos.classList.add("nav-menu-link_active");
@@ -40,7 +46,7 @@ hardware.addEventListener("click", ()  => {
 software.addEventListener("click", ()  => {
     software.classList.add("nav-menu-link_active");
     
-    incio.classList.remove("nav-menu-link_active")
+    incio.classList.remove("nav-menu-link_active") 
     perifericos.classList.remove("nav-menu-link_active")
     hardware.classList.remove("nav-menu-link_active")
     contacto.classList.remove("nav-menu-link_active")
